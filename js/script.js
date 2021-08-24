cbce26080ce7482b9c5817e6e8787f4bc062c147
angel23mc <86211355+angel23mc@users.noreply.github.com>
2021-08-23 21:29:39 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+//Form
+const form = document.querySelector("#contact-form");
+const errorsList = document.querySelector("#errors");
+
+form.addEventListener("submit", validateForm);
+
+function validateForm(event) {
+  // no back-end code
+  event.preventDefault();
+  let firstName = document.getElementById("first-name").value;
+  if (firstName.length < 2) {
+    console.log (firstName);
+    addError("First name must be at least 2 characters");
+  }
+  let lastName = document.querySelector("#last-name").value;
+  if (lastName.length < 2) {
+    addError("Last name must be at least 2 characters");
+  }
+  let phoneNumberValid =
+    document.querySelector("#phone-area-code").value.length === 3 &&
+    document.querySelector("#phone-prefix").value.length === 3 &&
+    document.querySelector("#phone-line-number").value.length === 4;
+  if (!phoneNumberValid) {
+    addError("Please provide a valid 10 digit number");
+  }
+  let areaInterest = document.querySelector("#area-interest");
+  if (areaInterest.value === "Select One") {
+    addError("Please select an area of interest.");
+  }
+  let additionalInfo = document.querySelector("#additional-info").value;
+  if ((areaInterest.value === "Other", pastExperience.length < 1)) {
+    addError(
+      "Please provide additional details to describe your 'Other' inquiry."
+    );
+  }
+}
+
+function addError(error) {
+  let li = document.createElement("li");
+  li.innerText = error;
+  errorsList.appendChild(li);
+}
+
+console.log(name);
